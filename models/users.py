4b0f6618ccc8a73482870cbd979fe5c9dc9eaad7
admin <romul1988@gmail.com>
2018-01-28 18:57:37 +0300

    правки

--- models/users.py
+++ models/users.py
@@ -2,17 +2,37 @@
 # -*- coding: utf-8 -*-
 # author: Roman B.
 
-from sqlalchemy import Table, Column, Integer, String, MetaData
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, MetaData, DateTime, ForeignKey, BigInteger
+from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
 from models.main import engine
 
+model = declarative_base(cls=DeferredReflection)
+
 metadata = MetaData()
-users_table = Table('lots', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('discord_mention', String, nullable=False),
-    Column('nick', String, nullable=True),
-    Column('item', String, nullable=False),
-    Column('price', String, nullable=True)
-)
+
+
+class Players(model):
+
+    __tablename__ = 'players'
+
+    id = Column(Integer, primary_key=True)
+    discord_mention = Column(String, nullable=False)
+    nickname = Column(String, nullable=True)
+    rank = Column(Integer, default=1)
+
+
+class TradeLots(model):
+
+    __tablename__ = 'trade_lots'
+
+    id = Column(Integer, primary_key=True)
+    owner_fid = Column(Integer, ForeignKey('players.id'), nullable=False),
+    url = Column(String, nullable=False)
+    item = Column(String, nullable=False)
+    price = Column(BigInteger, nullable=True)
+    time_created = Column(DateTime, default=datetime.now())
 
 if __name__ == '__main__':
-    metadata.create_all(engine)
+    print('окей')
